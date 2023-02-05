a3f2db93ff3cbeccfefb814ab0693729d1e1641a
Ines Rojas <45639602+InesR21@users.noreply.github.com>
2023-02-05 10:46:00 -0300

    add ScrollToIndexScreen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import RingIndicatorScreen from "./screens/RingIndicatorScreen";
 import LoadingDarkScreen from "./screens/LoadingDarkScreen";
 import SwitchScreen from "./screens/SwitchScreen";
+import ScrollToIndexScreen from "./screens/ScrollToIndexScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -10,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator
-        initialRouteName="LoadingDark"
+        initialRouteName="ScrollToIndex"
         screenOptions={{
           headerShown: false,
         }}
@@ -18,6 +19,7 @@ export default function App() {
         <Drawer.Screen name="RingIndicator" component={RingIndicatorScreen} />
         <Drawer.Screen name="LoadingDark" component={LoadingDarkScreen} />
         <Drawer.Screen name="Switch" component={SwitchScreen} />
+        <Drawer.Screen name="ScrollToIndex" component={ScrollToIndexScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
